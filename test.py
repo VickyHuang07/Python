d7b0d872f5cd4a66e466264b698309dd4f31e3aa
Vicky Huang <eunice82729@gmail.com>
2024-05-15 17:10:12 +0800

    upload test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+# print('超級大豬豬')
+
+# x = 5
+# print(x)
+
+# apple = 1
+# pen = 0.38
+# rainingoutside = False
+# name = 'Vicky'
+# print(apple)
+# print(pen)
+# print(rainingoutside)
+# print(name)
+# print(apple, pen, rainingoutside, name)
+
+name = input('請輸入名字: ')
+print('嗨', name)
